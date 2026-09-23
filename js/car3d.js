11d23fc99f623133a6720e5a98db66fcf8b1b884
Mohamed Fazil <1mmfazilprofessional@gmail.com>
2026-09-23 21:23:40 +0530

    Use optimized BMW M5 showroom model

--- js/car3d.js
+++ js/car3d.js
@@ -303,7 +303,7 @@
   const loader = new THREE.GLTFLoader();
    
 const MODEL_URL =
-  'assets/models/bmw_m5_showroom.glb';
+  'assets/models/bmw_m5_showroom_optimized_50.glb';
  
   loader.load(
     MODEL_URL,
